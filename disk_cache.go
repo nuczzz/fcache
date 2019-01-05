a11d766eecbeacb4b2eb3bcdac2e825d0e3e7066
nuczzz <nuc_zzz@163.com>
2019-01-05 17:09:31 +0800

    disk cache init

--- disk_cache.go
+++ disk_cache.go
@@ -1,6 +1,18 @@
 package hpcache
 
+import (
+	"io/ioutil"
+)
+
+type diskData struct {
+}
+
 type diskCache struct {
+	// dir directory of disk cache
+	dir string
+
+	// m map of disk cache data
+	m map[string]*diskData
 }
 
 func (dc *diskCache) Set(key string, value []byte) {
@@ -8,6 +20,22 @@ func (dc *diskCache) Set(key string, value []byte) {
 }
 
 func (dc *diskCache) Get(key string) []byte {
+	return nil
+}
+
+// init read disk cache info when create new disk cache
+func (dc *diskCache) init() error {
+	files, err := ioutil.ReadDir(dc.dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		//todo
+	}
+
 	return nil
 }
 
