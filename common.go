8b2eecc9e523402776baa218fbb35fc5225b387b
nuczzz <nuc_zzz@163.com>
2019-01-29 15:19:25 +0800

    optimize md5 function

--- common.go
+++ common.go
@@ -3,6 +3,7 @@ package fcache
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"unsafe"
 )
 
 const (
@@ -13,8 +14,18 @@ const (
 	DefaultDiskCacheDir     = "./cache/"
 )
 
+func String2Bytes(str string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&str))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func Bytes2String(bs []byte) string {
+	return *(*string)(unsafe.Pointer(&bs))
+}
+
 func MD5(src string) string {
 	ctx := md5.New()
-	ctx.Write([]byte(src))
+	ctx.Write(String2Bytes(src))
 	return hex.EncodeToString(ctx.Sum(nil))
 }
